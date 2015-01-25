d943c47cc1945c72ca790ed935c8342cbc07c204
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2015-01-25 21:28:02 +0300

    Add Kendall's tau implementation

--- src/estimators/estkendall.cpp
+++ src/estimators/estkendall.cpp
@@ -1,3 +1,16 @@
 #include "../data.h"
 
-extern "C" double exec(cst::data samples) { return 0; }
+static short sgn(double val) { return (0 < val) - (val < 0); }
+
+extern "C" double exec(cst::data data) {
+  long num = 0;
+  for (cst::ul i = 1; i < data.size(); ++i) {
+    for (cst::ul j = 0; j < i - 1; ++j) {
+      num += sgn(data[i][0] - data[j][0]) * sgn(data[i][1] - data[j][1]);
+    }
+  }
+
+  double denom = data.size() * (data.size() - 1) / 2;
+
+  return num / denom;
+}
