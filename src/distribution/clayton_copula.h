1b7489e009e13c5cf069be3300942a29bf9fab48
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2015-06-15 13:51:49 +0300

    Implement Frank and Clayton copulas

---
+++ src/distribution/clayton_copula.h
@@ -0,0 +1,37 @@
+//
+//  clayton_copula.h
+//  corrstat
+//
+//  Created by Ivan Dmitrievsky on 13/06/15.
+//  Copyright (c) 2015 Иван Дмитриевский. All rights reserved.
+//
+
+#ifndef corrstat_clayton_copula_h
+#define corrstat_clayton_copula_h
+
+#include <cmath>
+
+#include "acopula.h"
+
+namespace cst {
+
+class clayton_copula_t : public acopula_t {
+public:
+  clayton_copula_t(size_t dim, num_t theta) : acopula_t(dim), _theta(theta) {
+    assert(-1 <= theta && theta != 0 && theta < INFINITY);
+  }
+
+  virtual num_t gen(num_t x) const {
+    return (std::pow(x, -_theta) - 1) / _theta;
+  }
+
+  virtual num_t inv_gen(num_t x) const {
+      return std::pow(_theta * x + 1, -1 / _theta));
+  }
+
+private:
+  num_t _theta;
+};
+}
+
+#endif
---
+++ src/distribution/frank_copula.h
@@ -0,0 +1,37 @@
+//
+//  frank_copula.h
+//  corrstat
+//
+//  Created by Ivan Dmitrievsky on 13/06/15.
+//  Copyright (c) 2015 Иван Дмитриевский. All rights reserved.
+//
+
+#ifndef corrstat_frank_copula_h
+#define corrstat_frank_copula_h
+
+#include <cmath>
+
+#include "acopula.h"
+
+namespace cst {
+
+class frank_copula_t : public acopula_t {
+public:
+  frank_copula_t(size_t dim, num_t theta) : acopula_t(dim), _theta(theta) {
+    assert(theta != 0);
+  }
+
+  virtual num_t gen(num_t x) const {
+    return -std::log((std::exp(-_theta * x) - 1) / (std::exp(-_theta) - 1));
+  }
+
+  virtual num_t inv_gen(num_t x) const {
+    return -std::log((std::exp(-x) - 1) * (std::exp(-_theta) - 1) + 1) / _theta;
+  }
+
+private:
+  num_t _theta;
+};
+}
+
+#endif
