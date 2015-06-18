8ae968aea7a9058730d1cdd0d78e3e41d8229436
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2015-06-18 20:23:39 +0300

    Override pure functions in distribution to be pure

--- src/distribution/distribution.h
+++ src/distribution/distribution.h
@@ -14,7 +14,22 @@
 namespace cst {
 
 class distribution_t : public distribution_i {
+public:
   virtual num_t prob(num_t x) const { return 0; }
+  virtual num_t density(num_t x) const = 0;
+  virtual num_t cdf(num_t x) const = 0;
+  virtual num_t inv_cdf(num_t p) const = 0;
+
+  virtual num_t mean() const = 0;
+  virtual num_t variance() const = 0;
+
+  virtual num_t supp_lower_bound() const = 0;
+  virtual num_t supp_upper_bound() const = 0;
+
+  virtual bool is_supp_lower_bound_inclusive() const = 0;
+  virtual bool is_supp_upper_bound_inclusive() const = 0;
+
+  virtual num_t sample() const = 0;
 };
 }
 
