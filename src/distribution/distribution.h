2e0e42e8866b828c1ee918a64982b1adae815356
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2015-06-20 19:03:16 +0300

    Change hierarchy of empirical distributions

--- src/distribution/distribution.h
+++ src/distribution/distribution.h
@@ -9,6 +9,10 @@
 #ifndef __corrstat__distribution__
 #define __corrstat__distribution__
 
+#include <cerrno>
+#include <ctime>
+#include <fstream>
+
 #include "distribution_i.h"
 
 namespace cst {
@@ -30,6 +34,22 @@ public:
   virtual bool is_supp_upper_bound_inclusive() const = 0;
 
   virtual num_t sample() const = 0;
+
+  result<void*> export_cdf(std::string path_to_data) {
+    std::ofstream stream;
+    stream.open(path_to_data.c_str(), std::ofstream::trunc);
+
+    if (stream.fail()) {
+      return result<void*>::error(strerror(errno), error_t::io_error);
+    }
+
+    vec_t sample;
+    for (size_t i = 0; i < _grid.size(); ++i) {
+      stream << _grid[0][i] << ' ' << cdf(_grid[0][i]) << '\n';
+    }
+    stream.close();
+    return result<void*>::ok(NULL);
+  }
 };
 }
 
