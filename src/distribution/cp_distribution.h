3a83716d01905f7f5daedea7e1d90ffabf4c1ede
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2015-06-21 14:24:29 +0300

    Add tied_grid to distribution created with copula

--- src/distribution/cp_distribution.h
+++ src/distribution/cp_distribution.h
@@ -42,6 +42,23 @@ public:
     return _margins[coordinate]->cdf(x);
   }
 
+  void set_tied_grid() {
+    _tied_grid.clear();
+    std::vector<vec_t> margins;
+
+    for (size_t i = 0; i < _margins.size(); ++i) {
+      vec_t uv;
+      for (size_t k = 0; k < _margins.size(); ++k) {
+        uv.push_back(_margins[i]->grid()[k][0]);
+      }
+      margins.push_back(uv);
+    }
+
+    rec_dist(margins, 0, _margins.size(), vec_t());
+  }
+
+  void set_tied_grid_as_grid() { _grid = _tied_grid; }
+
   virtual vec_t sample() const {
     // TODO: Implement if needed.
     assert(false);
@@ -49,7 +66,21 @@ public:
   }
 
 private:
+  void rec_dist(const std::vector<vec_t>& margins, size_t coordinate,
+                size_t dim, vec_t point) const {
+    point.push_back(0);
+    for (size_t i = 0; i < margins[coordinate].size(); ++i) {
+      point[coordinate] = margins[coordinate][i];
+      if (coordinate + 1 < dim) {
+        rec_dist(margins, coordinate + 1, dim, point);
+      } else {
+        _tied_grid.push_back(point);
+      }
+    }
+  }
+
   const copula_t* _copula;
+  mutable std::vector<vec_t> _tied_grid;
   std::vector<const distribution_t*> _margins;
 };
 }
