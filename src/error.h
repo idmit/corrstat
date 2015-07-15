4250ad189b983afc6c611447720c93e8f00c29c2
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2015-07-15 13:59:41 +0300

    Document error module

--- src/error.h
+++ src/error.h
@@ -11,12 +11,38 @@
 
 #include <string>
 
+/**
+ *  This module contains definition of the error type.
+ */
+
+/**
+ *  Error class storing cause of an error and its type.
+ */
 class error_t {
 public:
+  /**
+   Different types of representable errors.
+   */
   enum type { runtime_error, io_error };
 
+  /**
+   *  Constructor accepting error cause and type, which defaults to runtime
+   *  error.
+   */
   error_t(const std::string &what, error_t::type type = runtime_error);
+
+  /**
+   *  Getter for the cause field.
+   *
+   *  @return Description of the error cause.
+   */
   const std::string &what() const;
+
+  /**
+   *  Getter for the error type.
+   *
+   *  @return Type of the error.
+   */
   error_t::type how() const;
 
 private:
