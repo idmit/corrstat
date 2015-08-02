33d3cc96be19993bd41a6d8317840b30e12816ce
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2015-08-02 22:10:35 +0300

    Document mv distribution interface module

--- src/distribution/mv_distribution_i.h
+++ src/distribution/mv_distribution_i.h
@@ -11,16 +11,66 @@
 
 #include "../types.h"
 
+/**
+ *  This module contains definition of the mv_distribution interface.
+ */
+
 namespace cst {
 
+/**
+ *  mv_distribution interface provides a set of common operations over
+ *  multivariate distribution.
+ */
 class mv_distribution_i : public mv_grid_i {
 public:
+  /**
+   *  Get a value of probability density function at given argument x.
+   *
+   *  @param x Argument of interest.
+   *
+   *  @return Value of probability density function at given argument.
+   */
   virtual num_t density(const vec_t& x) const = 0;
+
+  /**
+   *  Get a value of cumulative distribution function at given argument x.
+   *
+   *  @param x Argument of interest.
+   *
+   *  @return Value of cumulative probability density function at given
+   *  argument.
+   */
   virtual num_t cdf(const vec_t& x) const = 0;
+
+  /**
+   *  Get a value of marginal cumulative distribution function at given
+   *  argument x.
+   *
+   *  @param coordinate Coordinate corresponding to margin.
+   *  @param x          Argument of interest.
+   *
+   *  @return Value of marginal cumulative distribution function at given
+   *  argument.
+   */
   virtual num_t margin_cdf(size_t coordinate, num_t x) const = 0;
+
+  /**
+   *  Get the distribution's dimensions.
+   *
+   *  @return Number of dimensions.
+   */
   virtual size_t dim() const = 0;
+
+  /**
+   *  Get a sample.
+   *
+   *  @return Sample value.
+   */
   virtual vec_t sample() const = 0;
 
+  /**
+   *  Virtual destructor for building hierarchy.
+   */
   virtual ~mv_distribution_i(){};
 };
 }
